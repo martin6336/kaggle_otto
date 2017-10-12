cae4e948d1b31caa9461686fe9949d5eb119fe5d
martin6336 <beerbull@126.com>
2017-10-12 22:19:13 +0800

    Update blender.py

--- otto/otto_utils/blender.py
+++ otto/otto_utils/blender.py
@@ -12,14 +12,14 @@ import utils
 def blended(c, x):
     result = None
     for i in range(len(c)):
-        result = result + c[i] * x[i] if result is not None else c[i] * x[i]
+        result = result + c[i] * x[i] if result is not None else c[i] * x[i]  # 把所有矩阵（预测的概率）按照权重加权，然后除以总的权重标准化。
     result /= sum(c)
     return result
 
 
 def error(p, x, y):
     preds = blended(p, x)
-    err = log_loss(y, preds)
+    err = log_loss(y, preds)  # 看来一维向量是可以和一个矩阵直接比较求logloss的
     return err
 
 
@@ -44,7 +44,7 @@ def get_weights():
     # Minimize blending function
     p0 = [1.] * len(prediction_files)
     p = fmin_cobyla(error, p0, args=(val_predictions, val_labels), cons=[constraint], rhoend=1e-5)
-
+    #  损失函数，初始值，损失函数的两个确定的位置的值，限制函数>0，rhoend
     return p
 
 
